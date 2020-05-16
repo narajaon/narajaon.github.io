71dfbbb8d9bf20d0e38e296a41a228c72b78469b
narajaon <rajaonarisonfabien@yahoo.fr>
2020-05-16 17:38:13 +0200

    feat: added beta logo

--- components/HomeBanner.vue
+++ components/HomeBanner.vue
@@ -17,7 +17,9 @@ export default {
       narajaon's
     </h5>
     <h1 class="banner-title">
-      Website Portfolio
+      Website Portfolio<p class="beta">
+        beta
+      </p>
     </h1>
     <p class="banner-content">
       Hello stranger, welcome to my portfolio ! Below are some of the most noteworthy projects I did so enjoy.
@@ -36,5 +38,16 @@ export default {
 
 .banner-owner {
     transition: color 1s;
+}
+
+.beta {
+    position: absolute;
+    top: 54px;
+    left: 253px;
+    font-size: 16px;
+    color: red;
+    border: 1px solid red;
+    padding: 3px;
+    font-weight: initial;
 }
 </style>
